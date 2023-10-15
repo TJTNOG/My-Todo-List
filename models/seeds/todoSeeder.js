98e79fe7adcfecc376e82a2bdffcd5900ae755b5
Jin <jttang29@gmail.com>
2023-10-15 21:43:23 +0800

    feat: add handlebars

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
+
 const db = mongoose.connection
 
 db.on('error', () => {
